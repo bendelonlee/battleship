991b99f99b0cd3419b59f3f1a5d019ae2a81030e
Ben Lee <bendelonlee@gmail.com>
2018-10-09 07:54:11 -0600

    Lot of code no tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,8 @@
 require './lib/ship'
 class Board
+
+  OPPOSITE = {x: :y, y: :x}
+
   attr_reader :ships, :guesses
 
   def initialize(width, height)
@@ -10,15 +13,47 @@ class Board
   end
 
   #command_methods
-  def add_ship(coords)
-    info = validate_coords(coords)
-    if info == :valid
+  def add_ship(start_coord, end_coord)
       @ships << Ship.new(coords)
-    end
-    return info
   end
 
   #query_methods
+
+  def space_for_ship_at_start_coord?(start_coord, ship_size)
+    get_possible_end_coords(start_coord, ship_size).any?
+  end
+
+  def get_possible_end_coords(start_coord, ship_size)
+    get_end_coords_in_board(start_coord, ship_size).select do |end_coord|
+      space_for_ship?(get_conseq_coords(start_coord, end_coord))
+    end
+  end
+
+  private
+
+  def get_end_coords_in_board(start_coord, ship_size)
+    result = []
+    start_coord.each do |k,v|
+      [v + 4, v - 4].each do |i|
+         result << {k => i} if i <= @width
+      end
+    end
+    result
+  end
+
+  def get_conseq_coords(start_coord, end_coord)
+    result = []
+    x_or_y = x_or_y_in_line(coords)
+    line_val = start_coord.x_or_y
+    coords = [start_coord, end_coord]
+    get_range_from_coords(coords, OPPOSITE[x_or_y]).each do |i|
+      result << {x_or_y => line_val, OPPOSITE[x_or_y] => i}
+    end
+  end
+
+  def get_range_from_coords(coords, x_or_y)
+    (coords[0].x_or_y..coords[1].x_or_y)
+  end
 
   def validate_coords(coords)
     return :intersecting unless space_for_ship?(coords)
@@ -27,17 +62,21 @@ class Board
     return :valid
   end
 
-  def coords_conseq?(coords)
-    xvals = coords.map{|c| c[:x]}
-    yvals = coords.map{|c| c[:y]}
-    xvals.uniq.size == 1 || yvals.uniq.size == 1
+  def x_or_y_in_line(coords)
+    return :x if in_line?(:x, coords)
+    return :y if in_line?(:y, coords)
+    false
   end
 
-  def coords_in_range?(coords)
-    coords.each {|c| return false unless coord_in_range?(c) }
+  def in_line?(x_or_y, coords)
+    coords.map{|c| c[x_or_y]}.unique.size == 1
+  end
+
+  def coords_in_board?(coords)
+    coords.each {|c| return false unless coord_in_board?(c) }
     true
   end
-  def coord_in_range?(coord)
+  def coord_in_board?(coord)
     coord[:x] < (@width) && coord[:y] < (@height)
   end
 
