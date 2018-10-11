fd2a7bf449ec8128f509ce9e6817307b1a16b180
Ben Lee <bendelonlee@gmail.com>
2018-10-11 13:30:06 -0600

    Adds validation methods to game class. Gameplay works and validation works. Game class has gotten too long and needs to be refactored. Many methods that could have tests don't

--- test/game_test.rb
+++ test/game_test.rb
@@ -9,10 +9,15 @@ class GameTest < Minitest::Test
 
   def test_is_letter_number
     assert @game.is_letter_number?('A1')
-    assert @game.is_letter_number?('AA99')
-    assert @game.is_letter_number?('ZZ90')
+    assert @game.is_letter_number?('A99')
+    assert @game.is_letter_number?('Z90')
     # refute @game.is_letter_and_number?('A1A1')
     # needs to be improved for somthing like this
+    refute @game.is_letter_number?('1A')
+  end
+
+  def test_unguessed
+    #should write
   end
 
 end
